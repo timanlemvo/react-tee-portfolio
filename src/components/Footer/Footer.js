50db79b674d899561d0ff25167fce2ea4a5969a2
timanlemvo <timanlemvo@gmail.com>
2021-10-31 20:00:00 -0700

    half way done

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+
+const FooterSection = styled.div`
+  background-image: url(https://raw.githubusercontent.com/gurupawar/portfolio/main/assets/footer_wave.svg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;
+  height: 250px;
+  position: relative;
+
+  span {
+    position: absolute;
+    bottom: 4rem;
+    color: #fff;
+
+    a {
+      text-decoration: underline;
+    }
+  }
+`;
+function Footer() {
+  return (
+    <FooterSection>
+      <div className="Container">
+        <span>
+          Coded by{" "}
+          <a
+            href="https://github.com/timanlemvo//"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @timanlemvo
+          </a>{" "}
+        </span>
+      </div>
+    </FooterSection>
+  );
+}
+
+export default Footer;
---
+++ src/data/ProjectData.js
@@ -0,0 +1,70 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "https://raw.githubusercontent.com/timanlemvo/t-range-cal/main/src/assets/Preview.png",
+    title: "Tesla's Battery Range Calculator",
+    description: "Tesla's Battery Range Calculator built using React.JS, Redux and a few animations.",
+    tech_stack: "HTML, CSS, JavaScript, ReactJS, Animista",
+    github_url: "https://github.com/timanlemvo/t-range-cal",
+    demo_url: "https://pensive-kepler-4f372d.netlify.app/",
+  },
+  {
+    img: "https://raw.githubusercontent.com/timanlemvo/Tee-Portfolio/main/assets/images/run-buddy.png",
+    title: "Run Buddy",
+    description:
+      "A single page web site that offers fitness training services.",
+    tech_stack: "HTML & CSS",
+    github_url: "https://github.com/timanlemvo/run-buddy",
+    demo_url: "https://lernantino.github.io/run-buddy/",
+  },
+  {
+    img: "https://gurupawar.github.io/portfolio/assets/portfolio.png",
+    title: "Portfolio",
+    description: "Ofcourse i made my own personal portfolio website.",
+    tech_stack: "Reactjs, Css",
+    github_url: "https://github.com/gurupawar/portfolio-react",
+    demo_url: "https://gportfolio.vercel.app/",
+  },
+  {
+    img: "https://gurupawar.github.io/portfolio/assets/project_3.png",
+    title: "Ecommerce website",
+    description:
+      "Created simple ecommerce website using reactjs and react-use-cart plugin.",
+    tech_stack: "Reactjs, Css",
+    github_url: "https://github.com/gurupawar/ecommerce-react-app",
+    demo_url: "https://ecommerce-react-app.vercel.app/",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://gurupawar.github.io/portfolio/assets/skill/html5.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://gurupawar.github.io/portfolio/assets/skill/css3.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://gurupawar.github.io/portfolio/assets/skill/javascript.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://gurupawar.github.io/portfolio/assets/skill/bootstrap.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://gurupawar.github.io/portfolio/assets/skill/sass.svg",
+    name: "Sass/Scss",
+  },
+  {
+    img: "https://gurupawar.github.io/portfolio/assets/skill/git.svg",
+    name: "Git",
+  },
+  {
+    img: "https://gurupawar.github.io/portfolio/assets/skill/responsive.svg",
+    name: "Responsive",
+  },
+];
